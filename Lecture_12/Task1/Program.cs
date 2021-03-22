499155a432193b853fa978f054684b1c334f0ba2
Arthur Shmigelski <arthur19shmigelski@gmail.com>
2021-03-22 19:27:48 +0300

    Lecture 12 First commit

---
+++ Lecture_12/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueItem uniq1 = new UniqueItem();
+            UniqueItem uniq2 = new UniqueItem();
+            UniqueItem uniq3 = new UniqueItem();
+            UniqueItem uniq4 = new UniqueItem();
+            UniqueItem.DisplayId();
+        }
+    }
+    class UniqueItem
+    {
+        private static int Id = 0;
+        public UniqueItem()
+        {
+            Id++;
+        }
+        public static void DisplayId()
+        {
+            Console.WriteLine($"Create {Id} - instance.");
+        }
+    }
+}
