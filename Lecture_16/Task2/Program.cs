945b4ce7a5802a53650d92e3c4dc3016295d64e4
Arthur Shmigelski <arthur19shmigelski@gmail.com>
2021-04-05 23:15:16 +0300

    commit 2

--- Lecture_16/Task2/Program.cs
+++ Lecture_16/Task2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Task2
 {
@@ -6,11 +8,39 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Product product = new Product();
+            
+            Type typeProduct = typeof(Product);
+            ConstructorInfo ctor = typeProduct.GetTypeInfo()
+                .DeclaredConstructors.Single();
 
+            Object[] parameters = new Object[0];
+            object obj = ctor.Invoke(parameters);
+            Console.WriteLine($"Created is {obj.GetType()}");
+
+            FieldInfo fieldInfo = obj.GetType().GetTypeInfo().GetDeclaredField("_id");
+            Console.WriteLine($"Field Name is {fieldInfo.Name}");
+            Console.WriteLine($"Is Private - {fieldInfo.IsPrivate}");
+            Console.WriteLine(obj.ToString());
+            
+            fieldInfo.SetValue(obj, Guid.NewGuid());
+            Console.WriteLine(obj.ToString());
+
+            PropertyInfo propertyInfo = obj.GetType().GetTypeInfo().GetDeclaredProperty("Name");
+            propertyInfo.SetValue(obj, "Iphone 12 PRo");
+            Console.WriteLine(obj.ToString());
+            FieldInfo fieldInfo1 = obj.GetType().GetTypeInfo().GetDeclaredField("ProductType");
+            Console.WriteLine($"Field Name is {fieldInfo1.Name}");
+            Console.WriteLine($"Is Private - {fieldInfo1.IsPrivate}");
+
+            //С константой не понятно
+            FieldInfo fieldInfo3 = obj.GetType().GetTypeInfo().GetDeclaredField("ProductType");
+            object obj3 = fieldInfo.GetRawConstantValue();
+            obj3 = 123;
+            Console.WriteLine(obj.ToString());
+           
         }
     }
-    class Product
+    public class Product
     {
         public const string ProductType = "Phone";
         private Guid _id;
@@ -21,6 +51,10 @@ namespace Task2
         public Product()
         {
             _id = Guid.NewGuid();
+        }
+        public override string ToString()
+        {
+            return $"Your Id = {Id}, Name - {Name}.";
         }
     }
 }
