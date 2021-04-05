a69cc8af92aadd36f42f5953747e879a1c2d0d18
Arthur Shmigelski <arthur19shmigelski@gmail.com>
2021-04-05 21:40:37 +0300

    Lecture_16_first commit

---
+++ Lecture_16/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Option 1 (string value) - передал string в метод
+            MyClass my = new MyClass();
+            my.DeleteCharAfterFive("Input youre name: ");
+            #endregion
+
+            #region Option 2 (StringBuilder) -  передал StringBuilder в метод
+            StringBuilder sb1 = new StringBuilder();
+            sb1.DeleteCharAfterFive("Input youre name: ");
+            #endregion
+
+        }
+    }
+    internal class MyClass
+    {
+        internal string _name;
+    }
+    static class ExtensionClass
+    {
+        public static void DeleteCharAfterFive(this MyClass instance, string text)
+        {
+            Console.Write(text);
+            instance._name = Console.ReadLine();
+            const int firstFiveLetters = 5;
+            instance._name = instance._name.Remove(firstFiveLetters, instance._name.Length - firstFiveLetters) + "...";
+            Console.WriteLine("Total value - " + instance._name);
+        }
+    }
+    public static class StringBuilderExtensions
+    {
+        public static void DeleteCharAfterFive(this StringBuilder sb, string text)
+        {
+            Console.Write(text);
+            const int firstFiveLetters = 5;
+            sb = new StringBuilder(Console.ReadLine());
+            sb.Remove(firstFiveLetters, sb.Length - firstFiveLetters);
+            sb.Insert(firstFiveLetters, "...");
+            Console.WriteLine("Total value - " + sb);
+        }
+    }
+}
---
+++ Lecture_16/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+        }
+    }
+    class Product
+    {
+        public const string ProductType = "Phone";
+        private Guid _id;
+        public Guid Id { get { return _id; } }
+
+        public string Name { get; set; }
+
+        public Product()
+        {
+            _id = Guid.NewGuid();
+        }
+    }
+}
